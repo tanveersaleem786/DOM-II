6d6461af861b7e0d77d57f3ef18db7f10f9c5524
tanveersaleem786 <tanveer.saleem786@gmail.com>
2019-12-06 22:06:39 -0500

    Few events have been done so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,84 @@
 // Your code goes here
+
+
+// Mouse over event on navigation.
+let navigation = document.querySelectorAll('nav a');
+
+for(let i = 0; i < navigation.length; i++)
+{
+    navigation[i].addEventListener("mouseover", (event) => {
+        
+        navigation[i].style.color = "#FEBA4D";
+        
+    })
+
+    navigation[i].addEventListener("mouseout", (event) => {
+        
+        navigation[i].style.color = "black";
+    })
+}
+
+let paragraph = document.querySelectorAll('p');
+
+// keydown event.
+window.addEventListener("keydown", (key) => {
+    
+    if(key.keyCode == "13")
+    {
+        for(let i = 0; i < paragraph.length; i++)
+        { 
+            paragraph[i].style.color = "black"; 
+        } 
+    }
+
+}, false);
+
+
+// load event.
+window.addEventListener("load", () => {
+
+    for(let i = 0; i < paragraph.length; i++)
+    { 
+        paragraph[i].style.color = "#FEBA4D"; 
+    }
+
+});
+
+const contentPick = document.querySelector('.content-pick');
+const destination = contentPick.querySelector('.destination');
+const signupBtn = destination.querySelector('.btn');
+
+/*
+contentPick.addEventListener('click', (event) => {
+   alert('Click 1: Content Pick Section Clicked!');
+});
+
+destination.addEventListener('click', (event) => {
+  alert('Click 2: Destination  Div Clicked!');
+  event.stopPropagation();
+});
+*/
+/*
+signupBtn.addEventListener('click', (event) => {
+  alert('Click 3: Signup Button Clicked!');
+  event.stopPropagation();
+});
+*/
+
+signupBtn.addEventListener('dblclick', (event) => {
+  alert('Click 3: Signup Button Double Clicked!');
+  event.stopPropagation();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
